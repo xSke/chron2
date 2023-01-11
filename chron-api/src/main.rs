0f31d273768e6d0ce9e127f39dafeffa7d0cd627
Astrid <me@astr.cc>
2023-01-11 23:42:40 +0100

    don't just bind to localhost

--- chron-api/src/main.rs
+++ chron-api/src/main.rs
@@ -42,7 +42,7 @@ async fn main() -> anyhow::Result<()> {
     .route("/v2/entities", get(api::get_entities))
     .with_state(state);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await?;
